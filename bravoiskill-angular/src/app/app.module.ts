8794e4a99311082960541d26010100f5e398ade4
Burca Lidia-Gabriela <burcalidia20@gmail.com>
2019-09-25 11:19:03 +0300

    Add login template

--- bravoiskill-angular/src/app/app.module.ts
+++ bravoiskill-angular/src/app/app.module.ts
@@ -5,13 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { UserService } from './user.service';
-
-
-
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
