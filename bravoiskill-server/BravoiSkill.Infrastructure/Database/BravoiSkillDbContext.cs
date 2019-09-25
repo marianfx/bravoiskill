8e63f85b738b4dedcdce2c124b48620b5b433f23
Burca Lidia-Gabriela <burcalidia20@gmail.com>
2019-09-25 10:30:27 +0300

    Update SQL insert
    Updated the SQL inserts and removed the code inserts from the ContextDb

--- bravoiskill-server/BravoiSkill.Infrastructure/Database/BravoiSkillDbContext.cs
+++ bravoiskill-server/BravoiSkill.Infrastructure/Database/BravoiSkillDbContext.cs
@@ -14,21 +14,6 @@ namespace BravoiSkill.Infrastructure.Database
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<Badge>().HasData(new Badge[] {
-                new Badge{Description="Cel mai nou"},
-                new Badge{Description="Cel mai avansat"},
-            });
-            modelBuilder.Entity<Profile>().HasData(new Profile[] {
-                new Profile{Description = "Administrator"},
-                new Profile{Description="User"},
-                new Profile{Description="Guest"}
-            });
-            modelBuilder.Entity<User>().HasData(new User[] {
-                new User{FirstName = "Andrei", LastName="Leahu", DateOfBirth = new System.DateTime(1996, 11, 29), Email="andrei_lh@yahoo.com", ProfileId=1},
-                new User{FirstName = "Lidia-Gabriela", LastName="Burca", DateOfBirth = new System.DateTime(1996, 11, 29), Email="lidia_bu@yahoo.com", ProfileId=2},
-                new User{FirstName = "Olimpia", LastName="Ticlos", DateOfBirth = new System.DateTime(1996, 11, 29), Email="olimpia_ti@yahoo.com", ProfileId=3}
-            });
         }
     }
 }
