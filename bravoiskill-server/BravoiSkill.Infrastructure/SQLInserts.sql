c741213fc733d09a087d4ad73d100139657f77bd
Andrei Leahu <andrei_lh@yahoo.com>
2019-10-02 12:13:13 +0300

    Update to the SQL Insersts indenting

--- bravoiskill-server/BravoiSkill.Infrastructure/SQLInserts.sql
+++ bravoiskill-server/BravoiSkill.Infrastructure/SQLInserts.sql
@@ -49,17 +49,17 @@ INSERT INTO BravoiSkill.dbo.Users (FirstName,LastName,DateOfBirth,Email,Password
 ;
 
 
-    --//modelBuilder.Entity<Badge>().HasData(new Badge[] {
-    --//    new Badge{Description="Cel mai nou"},
-    --//    new Badge{Description="Cel mai avansat"},
-    --//});
-    --//modelBuilder.Entity<Profile>().HasData(new Profile[] {
-    --//    new Profile{Description = "Administrator"},
-    --//    new Profile{Description="User"},
-    --//    new Profile{Description="Guest"}
-    --//});
-    --//modelBuilder.Entity<User>().HasData(new User[] {
-    --//    new User{FirstName = "Andrei", LastName="Leahu", DateOfBirth = new System.DateTime(1996, 11, 29), Email="andrei_lh@yahoo.com", ProfileId=1},
-    --//    new User{FirstName = "Lidia-Gabriela", LastName="Burca", DateOfBirth = new System.DateTime(1996, 11, 29), Email="lidia_bu@yahoo.com", ProfileId=2},
-    --//    new User{FirstName = "Olimpia", LastName="Ticlos", DateOfBirth = new System.DateTime(1996, 11, 29), Email="olimpia_ti@yahoo.com", ProfileId=3}
+	--//modelBuilder.Entity<Badge>().HasData(new Badge[] {
+	--//    new Badge{Description="Cel mai nou"},
+	--//    new Badge{Description="Cel mai avansat"},
+	--//});
+	--//modelBuilder.Entity<Profile>().HasData(new Profile[] {
+	--//    new Profile{Description = "Administrator"},
+	--//    new Profile{Description="User"},
+	--//    new Profile{Description="Guest"}
+	--//});
+	--//modelBuilder.Entity<User>().HasData(new User[] {
+	--//    new User{FirstName = "Andrei", LastName="Leahu", DateOfBirth = new System.DateTime(1996, 11, 29), Email="andrei_lh@yahoo.com", ProfileId=1},
+	--//    new User{FirstName = "Lidia-Gabriela", LastName="Burca", DateOfBirth = new System.DateTime(1996, 11, 29), Email="lidia_bu@yahoo.com", ProfileId=2},
+	--//    new User{FirstName = "Olimpia", LastName="Ticlos", DateOfBirth = new System.DateTime(1996, 11, 29), Email="olimpia_ti@yahoo.com", ProfileId=3}
 	--//});
