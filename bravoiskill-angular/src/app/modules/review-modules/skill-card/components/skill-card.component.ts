e198dbb2f1eaa5b8ac61cc70453be0fc90e6d7f3
Burca Lidia-Gabriela <burcalidia20@gmail.com>
2019-12-11 17:29:43 +0200

    Add colour to slider

--- bravoiskill-angular/src/app/modules/review-modules/skill-card/components/skill-card.component.ts
+++ bravoiskill-angular/src/app/modules/review-modules/skill-card/components/skill-card.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 import { Skill } from 'src/app/shared/shared-models/skill';
 
 @Component({
   selector: 'app-skill-card',
   templateUrl: './skill-card.component.html',
-  styleUrls: ['./skill-card.component.css']
+  styleUrls: ['./skill-card.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SkillCardComponent implements OnInit {
   val1: number;
@@ -15,4 +16,13 @@ export class SkillCardComponent implements OnInit {
   ngOnInit() {
   }
   
+  getClass() {
+    if(this.val1 >= 0 && this.val1<=3)
+      return 'red';
+    else if(this.val1 > 3 && this.val1<=6)
+      return 'yellow';
+    else if(this.val1 > 6 && this.val1<=8)
+      return 'green';
+    else return 'blue';
+  }
 }
